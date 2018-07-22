c2e02714a38d6d8782061642d584d1a808965a79
Harald Nordgren <haraldnordgren@gmail.com>
2018-07-22 18:07:38 +0200

    Betalo-productivity: Improve data formatting

--- python/betalo-productivity/ratios.py
+++ python/betalo-productivity/ratios.py
@@ -3,13 +3,27 @@
 class Ratio:
     def calculate_ratios(counter):
         total_prs = sum(counter.values())
-        ratios = {username: prs/total_prs for username, prs in counter.items()}
-        sorted_ratios = sorted(ratios.items(), reverse=True, key=lambda kv: kv[1])
+
+        ratios = {}
+        for username, prs in counter.items():
+            ratios[username] = {
+                "total": prs,
+                "ratio": prs/total_prs,
+            }
+        
+        sorted_ratios = sorted(
+            ratios.items(),
+            reverse=True,
+            key=lambda kv: kv[1]["ratio"],
+        )
         
         result = []
         for ratio_tuple in sorted_ratios:
             username, ratio = ratio_tuple
-            result.append((username, "{0:.0%}".format(ratio)))
+            result.append((username, "{0} ({1:.1%})".format(
+                ratio["total"],
+                ratio["ratio"],
+            )))
 
         return result
 
